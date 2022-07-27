973e3443848ae9ee9fdba1a0d39b33398c7bfa1b
Dafid Prasetyo <dafidpr@gmail.com>
2022-07-27 07:55:39 +0700

    add image

--- lib/services/api/rest_api_service.dart
+++ lib/services/api/rest_api_service.dart
@@ -4,9 +4,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String _baseUrl =
-      'https://api-presence-qrcode.herokuapp.com/api/v1/';
-  // static const String _baseUrl = 'http://10.0.2.2:8000/api/v1/';
+  // static const String _baseUrl =
+  //     'https://api-presence-qrcode.herokuapp.com/api/v1/';
+  static const String _baseUrl = 'http://10.0.2.2:8000/api/v1/';
   var _token;
 
   _getToken() async {
